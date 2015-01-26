1eaab95b16f97704e30640a451e1d40d80d26c5f
Ryan Tablada <ryan.tablada@gmail.com>
2015-01-26 11:08:45 -0600

    Add token handling

--- js/restful.js
+++ js/restful.js
@@ -1,11 +1,18 @@
 $(function(){
+	var tokenInput = '';
+
+	if (window.csrfToken) {
+		tokenInput = "<input type='hidden' name='_token' value='"+ window.csrfToken +"'>";
+	}
+
 	$('[data-method]').append(function(){
 		return "\n"+
-		"<form action='"+$(this).attr('href')+"' method='POST' style='display:none'>\n"+
-		"   <input type='hidden' name='_method' value='"+$(this).attr('data-method')+"'>\n"+
-		"</form>\n";
+			"<form action='"+$(this).attr('href')+"' method='POST' style='display:none'>\n"+
+			tokenInput +
+			"   <input type='hidden' name='_method' value='"+$(this).attr('data-method')+"'>\n"+
+			"</form>\n";
 	})
-	.removeAttr('href')
-	.attr('style','cursor:pointer;')
-	.attr('onclick','if(confirm("Are you sure?")){$(this).find("form").submit();}');
+		.removeAttr('href')
+		.attr('style','cursor:pointer;')
+		.attr('onclick','if(confirm("Are you sure?")){$(this).find("form").submit();}');
 });
