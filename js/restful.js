c3c7ba16e1a5383ef065da8fda562a710f318094
Ryan Tablada <ryan.tablada@gmail.com>
2015-01-27 16:09:13 -0600

    Allow for rerun of restful when needed

--- js/restful.js
+++ js/restful.js
@@ -1,18 +1,24 @@
-$(function(){
-	var tokenInput = '';
+var restful = (function(){
+	var restful = function() {
+		var tokenInput = null;
 
-	if (window.csrfToken) {
-		tokenInput = "<input type='hidden' name='_token' value='"+ window.csrfToken +"'>";
-	}
+		if (window.csrfToken) {
+			tokenInput = $("<input type='hidden' name='_token' value='"+ window.csrfToken +"'/>");
+		}
 
-	$('[data-method]').append(function(){
-		return "\n"+
-			"<form action='"+$(this).attr('href')+"' method='POST' style='display:none'>\n"+
-			tokenInput +
-			"   <input type='hidden' name='_method' value='"+$(this).attr('data-method')+"'>\n"+
-			"</form>\n";
-	})
-		.removeAttr('href')
-		.attr('style','cursor:pointer;')
-		.attr('onclick','if(confirm("Are you sure?")){$(this).find("form").submit();}');
-});
+		$('[data-method]').append(function(){
+			return "\n"+
+				"<form action='"+$(this).attr('href')+"' method='POST' style='display:none'>\n"+
+				tokenInput +
+				"   <input type='hidden' name='_method' value='"+$(this).attr('data-method')+"'>\n"+
+				"</form>\n";
+		})
+			.removeAttr('href')
+			.attr('style','cursor:pointer;')
+			.attr('onclick','if(confirm("Are you sure?")){$(this).find("form").submit();}');
+	};
+
+	restful();
+
+	return restful;
+})();
